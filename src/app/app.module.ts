bfd83ee8e2f16146628746c9a75ae483bc8daf1f
Aditya Mudgerikar <aditya18thm@gmail.com>
2020-07-13 22:28:46 +0530

    Added HTTP CLient Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,20 @@ import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 import Amplify from 'aws-amplify';
 import awsconfig from '../aws-exports';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 /* Configure Amplify resources */
 Amplify.configure(awsconfig);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AmplifyUIAngularModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AmplifyUIAngularModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
